de9ef728c49310b6f32ee0227174cbcc58bbae9d
natewong <nwwong17@gmail.com>
2021-09-25 12:23:41 -0700

    Created more array methods for APCS HW

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,26 +5,43 @@ public class MyMain {
     // Rotates all values in an array to the left
     // Ex. rotateLeft([1, 2, 3, 4, 5]) => [2, 3, 4, 5, 1]
     public static int[] rotateLeft(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int last = arr[0];
+        for (int i = 0; i < arr.length-1; i++) {
+            arr[i]=arr[i+1];
+        }
+        arr[arr.length-1]=last;
+        return arr;
     }
 
     // Rotates all values in an array to the right
     // Ex. rotateRight([1, 2, 3, 4, 5]) => [5, 1, 2, 3, 4]
     public static int[] rotateRight(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int front = arr[arr.length-1];
+        for (int i=arr.length-1; i>0; i--) {
+            arr[i]=arr[i-1];
+        }
+        arr[0]=front;
+        return arr;
     }
 
     // Replaces all duplicate values with -1
     // Ex. duplicateReplacer([1, 2, 1, 2, 1, 1]) => [1, 2, -1, -1, -1, -1]
     public static int[] duplicateReplacer(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i]==arr[j])
+                {
+                    arr[j]=-1;
+                }
+            }
+        }
+        return arr;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        System.out.println(Arrays.toString(rotateLeft(new int[]{10,11,12,13,14})));
+        System.out.println(Arrays.toString(rotateRight(new int[]{10,11,12,13,14})));
+        System.out.println(Arrays.toString(duplicateReplacer(new int[]{4,1,6,1,3,5,4,4,7})));
     }
 }
